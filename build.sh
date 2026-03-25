e69ee58e13a5523b27aef97e45f4f5c635a25a00
Tharun <nunavaththarun103@gmail.com>
2026-03-25 07:11:55 +0530

    final admin fix

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py shell < create_admin.py
+
+echo "from django.contrib.auth.models import User; User.objects.filter(username='admin').delete(); User.objects.create_superuser('admin','admin@gmail.com','123456')" | python manage.py shell
