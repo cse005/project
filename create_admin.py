a1250e77bae054653cef50ae400a37a331954266
Tharun <nunavaththarun103@gmail.com>
2026-03-25 07:04:00 +0530

    final fix admin

--- create_admin.py
+++ create_admin.py
@@ -2,11 +2,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-user, created = User.objects.get_or_create(username="admin")
+# DELETE old user if exists (important)
+User.objects.filter(username="admin").delete()
 
-user.set_password("123456")
-user.is_staff = True
-user.is_superuser = True
-user.save()
+# CREATE fresh superuser
+User.objects.create_superuser(
+    username="admin",
+    email="admin@gmail.com",
+    password="123456"
+)
 
-print("Admin ready")
+print("Fresh superuser created")
